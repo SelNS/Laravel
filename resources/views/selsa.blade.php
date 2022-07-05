f65b49ab96bec2301bc32f8e4d3c025c9bdc9bf2
SelNS <mcselsans@gmail.com>
2022-07-05 14:51:22 +0700

    versi 1.0.0

---
+++ resources/views/selsa.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laporan6 Zels</title>
+</head>
+<body>
+    <h1>Laporan 6- PemrogramanWEB2</h1>
+
+    <div>
+        <h3>Nama : Selsa NS</h3>
+        <h3>NIM : 60200120001</h3>
+        <h3>Kelas : C</h3>
+    </div>
+</body>
+</html>
